3f4ff7eea65d54417d6b068e0bbae5a7f924cce7
Spazmic <jeffmala@hotmail.com>
2015-04-30 15:55:48 -0400

    Update imagefetcher.sh

--- imagefetcher.sh
+++ imagefetcher.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 wget -q "http://www.urltoimage.com/image.png" -O /opt/image.png
 cp /opt/image.png /path/to/webserver/
-
-:’
-Replace "http://www.urltoimage.com/image.png" for the url of the image.
-By default, this script will save the image.png in the /opt folder and then copy that image.png to the webserver.
-You can modify the receiving folder.
-‘
